de0fb9b67d12dc3b389ef4a526c518bfa91a5412
AbhinandhanaPU <abhinandhanapu81@gmail.com>
2025-03-01 00:38:34 +0530

    otp screen

---
+++ lib/app/views/auth_screens/otp_screen.dart
@@ -0,0 +1,118 @@
+import 'dart:developer';
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:houseboat_booking/app/utils/colors.dart';
+import 'package:houseboat_booking/app/widgets/colored_button.dart';
+import 'package:pinput/pinput.dart';
+
+class OtpScreen extends StatelessWidget {
+  const OtpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormState>();
+
+    return Scaffold(
+      body: Form(
+        key: formKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            spacing: 30,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Phone Verification',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                ),
+              ),
+              const Text.rich(
+                textAlign: TextAlign.center,
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "Enter the OTP sent to ",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.lightGrey,
+                      ),
+                    ),
+                    TextSpan(
+                      text: "+91 1234567890",
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Pinput(
+                length: 6,
+                showCursor: true,
+                defaultPinTheme: PinTheme(
+                  height: 55,
+                  width: 46,
+                  textStyle: TextStyle(
+                    fontSize: 22,
+                  ),
+                  decoration: BoxDecoration(
+                    color: AppColors.lightPrimary.withAlpha(22),
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(
+                      color: AppColors.lightPrimary.withAlpha(128),
+                    ),
+                  ),
+                ),
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                controller: TextEditingController(),
+                keyboardType: TextInputType.number,
+                onSubmitted: (value) async {},
+                onCompleted: (pin) => log(pin),
+              ),
+              CustomcoloredButton(
+                text: 'VERIFY & LOGIN',
+                height: 60,
+                width: double.infinity,
+                onTap: () {
+                  if (formKey.currentState!.validate()) {}
+                },
+              ),
+              Text.rich(
+                textAlign: TextAlign.center,
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "Didn't recieve the OTP?  ",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.lightGrey,
+                      ),
+                    ),
+                    TextSpan(
+                      text: "RESEND",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.lightPrimary,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          // function to resent otp
+                        },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/app/widgets/colored_button.dart
+++ lib/app/widgets/colored_button.dart
@@ -5,36 +5,42 @@ class CustomcoloredButton extends StatelessWidget {
   final String text;
   final double height;
   final double width;
+  final Function()? onTap;
+
   const CustomcoloredButton({
     super.key,
     required this.text,
     required this.height,
     required this.width,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: height,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-          colors: [
-            AppColors.lightPrimary,
-            AppColors.darkPrimary,
-          ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [
+              AppColors.lightPrimary,
+              AppColors.darkPrimary,
+            ],
+          ),
         ),
-      ),
-      child: Center(
-        child: Text(
-          text,
-          style: TextStyle(
-            color: AppColors.whiteColor,
-            fontSize: 20,
-            fontWeight: FontWeight.w600,
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: AppColors.whiteColor,
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
           ),
         ),
       ),
