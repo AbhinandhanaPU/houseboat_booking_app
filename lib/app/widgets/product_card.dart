311f3de491a7932b80f806cfc51fed5501fea86a
AbhinandhanaPU <abhinandhanapu81@gmail.com>
2025-03-07 23:09:34 +0530

    separated product container into a reusable productcard

---
+++ lib/app/widgets/product_card.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:houseboat_booking/app/utils/colors.dart';
+
+class ProductCard extends StatelessWidget {
+  final String productImage;
+  final String productName;
+  final String productPlace;
+  final int productPrice;
+  final VoidCallback onTap;
+
+  const ProductCard(
+      {super.key,
+      required this.productImage,
+      required this.productName,
+      required this.productPlace,
+      required this.productPrice,
+      required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 300,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: AppColors.backgroundSecondary,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(10),
+              ),
+              child: Image.asset(
+                productImage,
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: 150,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    productName,
+                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 3),
+                  Text(
+                    productPlace,
+                    style: TextStyle(
+                      color: AppColors.lightPrimary,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text.rich(
+                      TextSpan(
+                        children: [
+                          const TextSpan(
+                            text: "Price from ",
+                          ),
+                          TextSpan(
+                            text: '₹$productPrice  ',
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const TextSpan(
+                            text: "1 Night, 2 Adults",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
