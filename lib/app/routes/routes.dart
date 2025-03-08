829f0e4ea217d7300b7b90c3b9267af8613593ce
AbhinandhanaPU <abhinandhanapu81@gmail.com>
2025-03-08 23:58:11 +0530

    app routes

---
+++ lib/app/routes/routes.dart
@@ -0,0 +1,14 @@
+class AppRoutes {
+  static String welcomeScreen = '/welcomeScreen';
+  static String signUp = '/signUp';
+  static String logIn = '/logIn';
+  static String otpScreen = '/otpScreen';
+  static String bottomNavScreen = '/bottomNavScreen';
+  static String homeScreen = '/homeScreen';
+  static String search = '/search';
+  static String bookingHistory = '/bookingHistory';
+  static String paymentHistory = '/paymentHistory';
+  static String chatHome = '/chatHome';
+  static String wishList = '/wishList';
+  static String profile = '/profile';
+}
